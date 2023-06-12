85c4f642c706b0b1f35df565758174ef4b94ee57
tteg33 <jeremyyu2001@protonmail.com>
2023-06-13 01:28:42 +0800

    updated renderer helper

--- src/components/GltfRenderer.js
+++ src/components/GltfRenderer.js
@@ -4,29 +4,67 @@ import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
-import React from 'react';
+import React, {Component} from 'react';
+import Dropzone from './Dropzone';
 
-const ModelDefault = () =>{
+const {useState} = React;
+
+const fileToDataUri = (file) => new Promise((resolve, reject)=> {
+	const reader = new FileReader();
+	reader.onload = (event) => {
+		resolve(event.target.result)
+	};
+	reader.readAsDataURL(file);
+})
+
+export default function GltfRendererUpload() {
+
+	const [dataUri, setDataUri] = useState('')
+		const onChange = (file) => {
+			if (!file) {
+				setDataUri("./scene.glb");
+				return;
+			}
+			fileToDataUri(file).then(dataUri => {
+				setDataUri(dataUri)
+			})
+		}
+
+
+		const ModelDefault = () =>{
+			const gltf = useLoader(GLTFLoader, dataUri);
+			return (
+				<>
+					<primitive object={gltf.scene} scale={0.2}/>
+				</>
+			);
+		};
+
+		return (
+			<div className="GltfRenderer">
+				<Canvas>
+					<Suspense fallback={null}>
+						<ModelDefault/>
+						<OrbitControls enableDamping={false}/>
+						<Environment preset="sunset" background/>
+					</Suspense>
+				</Canvas>
+			</div>
+
+		);
+
+}
+
+/*const ModelDefault = () =>{
 const gltf = useLoader(GLTFLoader, "./scene.glb");
 return (
 	<>
 	<primitive object={gltf.scene} scale={0.2}/>
 	</>
 );
-};
+};*/
 
-const ModelCustom = (url) => {
-	const gltf = useLoader(GLTFLoader, url);
-	return (
-		<>
-		<primitive object = {gltf.scene} scale = {0.2}/>
-		</>
-//needs to be changed
-	);
-};
-
-
-export default function GltfRenderer() {
+/*export default function GltfRenderer() {
 	return (
 		<div className="GltfRenderer">
 		<Canvas>
@@ -39,7 +77,7 @@ export default function GltfRenderer() {
 		</div>
 
 	);
-}
+}*/
 
 
 
